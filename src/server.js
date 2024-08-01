f9a7f5c0a42c104d043c27c1bc825231644899c9
Giovanna Borges <grborgesp@gmail.com>
2024-07-31 22:15:57 -0300

    add import type module and hello world express

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
-const express = require('express')
+//const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // primeiro end-point
     res.send('Hello World!')
 })
 
+app.get('/user', (req, res) => { // segundo end-point
+    res.json({nome: "Giovanna", email: "grborgesp@gmail.com"})
+})
+
 app.listen(port, () => {
-    console.log('Servidor rodando em http://localhost:${port}')
+    console.log(`Servidor rodando em http://localhost:${port}`)
 })
