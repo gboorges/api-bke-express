e8cc459c15d8848f318bf510dbc22e8cd40b3d2a
Giovanna Borges <grborgesp@gmail.com>
2024-09-11 20:37:57 -0300

    change prisma

---
+++ database/api-bke-express.sql
@@ -0,0 +1,5 @@
+USE api_express;
+
+INSERT INTO user (name, email, pass) VALUES ('Giovanna', 'giovanna@gmail.com', '123@Teste');
+
+INSERT INTO products (modelo, marca, valor, quantidade) VALUES ('iPhone 15 Pro Max', 'Apple', '8599.00', '75');
