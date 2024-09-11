9440c5df2d7902cbcde75065da929aec7d29b1bb
Giovanna Borges <grborgesp@gmail.com>
2024-09-11 20:38:54 -0300

    change logger

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,11 +1,12 @@
 const logger = (req, res, next) => {
     // funções para extrair a data da requisição
-    const now = new Date()
+    /*const now = new Date()
     const yyyy = now.getFullYear();
     const mm = now.getMonth() + 1;
     const dd = now.getDate()
     
-    console.log(`${dd+'/'+mm+'/'+yyyy} ${req.method} ${req.url}`) // exibe a data, o método e a url da requisição
+    console.log(`${dd+'/'+mm+'/'+yyyy} ${req.method} ${req.url}`) // exibe a data, o método e a url da requisição */
+    console.log(`${new Date(Date.now()).toLocaleDateString()} ${req.method} ${req.url}`)
     next()
 }
 
